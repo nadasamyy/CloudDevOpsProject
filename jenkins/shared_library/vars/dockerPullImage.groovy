d03e3015a959a4bb1b662a6af5d027493b56a03a
nadasamyy <119449041+nadasamyy@users.noreply.github.com>
2024-12-28 02:18:29 +0000

    Update and rename dockerPullImage.groovy to dockerBuild.groovy

--- jenkins/shared_library/vars/dockerPullImage.groovy
+++
@@ -1,6 +0,0 @@
-// vars/dockerPullImage.groovy
-def call(String imageName, String buildNumber, String dockerRegistry) {
-    sh """
-        docker pull ${dockerRegistry}/${imageName}:${buildNumber}
-    """
-}
